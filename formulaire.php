0286f05e9591aeabfc43256ea6c2a046db6909d6
mudather5 <mudathermohamed50@gmail.com>
2018-09-09 02:35:09 +0200

    added code in result.php and formulaire.php

--- formulaire.php
+++ formulaire.php
@@ -10,18 +10,28 @@
     <form method="post" action="result.php" value="">
         <input type="text" name="nom" value="votre nom"><br>
         <input type="text" name="prenom" value="votre prénom"><br>
-        <p>Sex:</p>
-        <input type="radio" name="gender" value="Male">homme<br>
-        <input type="radio" name="gender" value="Female">femme<br>
-        <p>Age:</p>
-        <input type="number" name="age" value="âge"><br>
+        <p>Sex:
+        <input type="radio" name="gender" value="Male">
+        <label for="gender">homme</label>
+        <input type="radio" name="gender" value="Female">
+            <label for="gender">femme</label></p><br>
+        <p>Age:
+            <input type="number" name="age" value="age"></p><br>
         <p>loisirs:</p>
-        <input type="checkbox" name="loisir1" value="fotball">Foot<br>
-        <input type="checkbox" name="loisir2" value="Basket">Basket<br>
-        <input type="checkbox" name="loisire3" value="Curling">Curling<br>
-        <input type="submit" value="Enoyez">
+        <input type="checkbox" name="loisir1" value="fotball">
+            <label for="football">Foot</label><br>
+        <input type="checkbox" name="loisir2" value="Basket"><label for="loisirs2">Basket</label><br>
+        <input type="checkbox" name="loisire3" value="Curling">
+        <label for="loisirs3">Curling</label><br>
+        <input type="submit" value="Enoyez"><br>
+        
+        <input type="text" name="username" value=""><br>
+        <input type="password" name="password" value=""><br>
+        <input type="submit" value="envoyez">
     </form>
-
+    
+    
+    
 
 
 </body>
--- result.php
+++ result.php
@@ -1,24 +1,35 @@
 <?php
-//if (!empty($_GET['firstname']) AND !empty($_GET['lastname']) AND !empty($_GET['repeter']))
+$nom = $_POST['nom'];
+$prenom = $_POST['prenom'];
+//$sex = $_POST['sex'];
+$age = $_POST['age'];
+$loisirs = $_POST['loisirs'];
+
+echo 'nom: '.$nom.'<br>'.'prénom: '.$prenom.'<br>'.'Age: '.(int)$_POST['age'].' '.'ans'.(int)$loisirs;
+
+
+
+//if (isset($_POST['nom'])  AND isset($_POST['sex']) AND isset($_POST['prenom']) AND isset($_POST['Age']) AND isset($_POST['loisirs']))
 //
 //{
 //
-//    for ($i = 0; $i < $_GET['repeter']; $i++)
+//        echo $_POST['nom'].'<br/>'.$_POST['sex'].'<br/>'.$_POST['prenom'].'<br />'.$_POST['Age'].'<br/>'.$_POST['loisirs'];
 //
-//    {
 //
-//        echo 'Bonjour ' . $_GET['firstname'] . ' ' . $_GET['lastname'] . ' !<br />';
-//
-//    }
+//}
 //
 //else
 //
 //{
 //
-//   echo 'bonjour';
+// echo 'nom:'.$_POST['nom'].'<br/>'.'prénom:'.$_POST['prenom'].'<br />'.$_POST['sex'].'<br/>'.$_POST['Age'].'<br/>'.$_POST['loisirs'];  
 //
 //}
 
-$_POST['nom'];
-$_POST['prénom'];
+
+//$username = $_POST['username'];
+//$password = $_POST['password'];
+//
+//echo $username.'<br/>'.$password;
+// 
 ?>
